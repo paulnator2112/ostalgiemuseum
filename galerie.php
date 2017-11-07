730f100cccc4fb840426f6e3ba519f52edb3982b
paulnator2112 <paulnator2112@users.noreply.github.com>
2017-11-07 14:38:04 +0100

    Update galerie.php
    Updated Folder Structure

--- galerie.php
+++ galerie.php
@@ -15,15 +15,15 @@
 		</nav>
 		<div id="allImages">
 			<?php
-			$images = scandir("img/");
-			$ini = parse_ini_file("img/display.ini",1);
+			$images = scandir("img/gal/");
+			$ini = parse_ini_file("img/gal/display.ini",1);
 			foreach($images as $key => $value){
 				if(!($value == ".." or $value =="." or $value == "display.ini")){
 					$split = explode(".",$value);
 					if($ini["display"][$split[0]]){
 						echo '<div class="gallery">
-								<a target="_blank" href="img/'.$value.'">
-									<img src="img/'.$value.'" width="300" height="200">
+								<a target="_blank" href="img/gal/'.$value.'">
+									<img src="img/gal/'.$value.'" width="300" height="200">
 								</a>
 								<div class="desc">'.$ini["title"][$split[0]].'</div>
 							</div>';
