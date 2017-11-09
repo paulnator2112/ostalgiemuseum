32a90cd25e8f4bba1a5b94185903f12efc0bce65
paulnator2112 <paulnator2112@users.noreply.github.com>
2017-11-09 11:39:49 +0100

    Update galerie.php

--- galerie.php
+++ galerie.php
@@ -20,14 +20,12 @@
 			foreach($images as $key => $value){
 				if(!($value == ".." or $value =="." or $value == "display.ini")){
 					$split = explode(".",$value);
-					if($ini["display"][$split[0]]){
-						echo '<div class="gallery">
-								<a target="_blank" href="img/gal/'.$value.'">
-									<img src="img/gal/'.$value.'" width="300" height="200">
-								</a>
-								<div class="desc">'.$ini["title"][$split[0]].'</div>
-							</div>';
-					}
+					echo '<div class="gallery">
+							<a target="_blank" href="img/gal/'.$value.'">
+								<img src="img/gal/'.$value.'" width="300" height="200">
+							</a>
+						<div class="desc">'.$ini["title"][$split[0]].'</div>
+					</div>';
 				}
 			}
 			?>		
